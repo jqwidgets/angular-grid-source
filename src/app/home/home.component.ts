1d8f282f60fb1fb2bbb155e1fcb73fb0df2b6ff8
jQWidgets <info@jqwidgets.com>
2018-11-28 18:31:18 +0200

    Home Page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
-  constructor() { }
+    ngAfterViewInit() {
+        $('.jqx-grid').css('margin', 'auto');
 
-  ngOnInit() {
-  }
+        let buttons = document.getElementsByClassName('jqx-button');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].removeAttribute('type');
+        } 
+    }
 
+    source: any = {
+        datatype: 'xml',
+        datafields: [
+            { name: 'ProductName', type: 'string' },
+            { name: 'QuantityPerUnit', type: 'int' },
+            { name: 'UnitPrice', type: 'float' },
+            { name: 'UnitsInStock', type: 'float' },
+            { name: 'Discontinued', type: 'bool' }
+        ],
+        root: 'Products',
+        record: 'Product',
+        id: 'ProductID',
+        url: '../../assets/sampledata/products.xml'
+    };
+
+    dataAdapter: any = new jqx.dataAdapter(this.source);
+
+    cellsrenderer = (row: number, columnfield: string, value: string | number, defaulthtml: string, columnproperties: any, rowdata: any): string => {
+        if (value < 20) {
+            return '<span style="margin: 4px; float: ' + columnproperties.cellsalign + '; color: #ff0000;">' + value + '</span>';
+        }
+        else {
+            return '<span style="margin: 4px; float: ' + columnproperties.cellsalign + '; color: #008000;">' + value + '</span>';
+        }
+    };
+
+    columns: any[] = [
+        { text: 'Product Name', columngroup: 'ProductDetails', datafield: 'ProductName', width: 248 },
+        { text: 'Quantity per Unit', columngroup: 'ProductDetails', datafield: 'QuantityPerUnit', cellsalign: 'right', align: 'right' },
+        { text: 'Unit Price', columngroup: 'ProductDetails', datafield: 'UnitPrice', align: 'right', cellsalign: 'right', cellsformat: 'c2' },
+        { text: 'Units In Stock', datafield: 'UnitsInStock', cellsalign: 'right', cellsrenderer: this.cellsrenderer, width: 100 },
+        { text: 'Discontinued', columntype: 'checkbox', datafield: 'Discontinued', align: 'center' }
+    ];
+
+    columngroups: any[] = [
+        { text: 'Product Details', align: 'center', name: 'ProductDetails' }
+    ];
 }
