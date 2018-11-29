67eb7bf5806a2a98f5fb7c9c0accff46a870962f
jQWidgets <info@jqwidgets.com>
2018-11-29 14:42:08 +0200

    API Reference page

--- src/app/docs-components/api-reference/api-reference.component.ts
+++ src/app/docs-components/api-reference/api-reference.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $;
+declare var formatCode;
 
 @Component({
-  selector: 'app-api-reference',
-  templateUrl: './api-reference.component.html',
-  styleUrls: ['./api-reference.component.css']
+    selector: 'app-api-reference',
+    templateUrl: './api-reference.component.html',
+    styleUrls: ['./api-reference.component.css']
 })
-export class ApiReferenceComponent implements OnInit {
+export class ApiReferenceComponent implements AfterViewInit {
 
-  constructor() { }
+    ngAfterViewInit() {
+        $(".documentation-option-type-click").click(function (event) {
+            $(event.target).parents('tr').next().find(".property-content").toggle();
+        });
 
-  ngOnInit() {
-  }
+        let codeSpans = document.getElementsByClassName('code');
 
+        for (let i = 0; i < codeSpans.length; i++) {
+            codeSpans[i].innerHTML = formatCode(codeSpans[i].innerHTML);
+        }
+    }
 }
